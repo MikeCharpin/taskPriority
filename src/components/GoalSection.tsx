0cf7c97e9a05a0dde8d9e87761e07f7584eec6f2
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-01-09 14:27:24 -0500

    Adding cards.

--- src/components/GoalSection.tsx
+++ src/components/GoalSection.tsx
@@ -1,8 +1,14 @@
+import SimpleCard from "./SimpleCard";
 
 export function GoalSection() {
     return (
-        <div className="w-1/3 border-2 border-blue-300">
-            Goal Section
+        <div className="flex flex-col gap-4 justify-center items-center p-4 border-2 border-blue-300 rounded-md">
+            <h1 className="text-xl font-bold">Goal Section</h1>
+            <SimpleCard />
+            <SimpleCard />
+            <SimpleCard />
+            <SimpleCard />
+            
         </div>
     )
 }
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,9 +1,15 @@
+import ComplexCard from "./ComplexCard";
 
 
 export default function ProjectSection() {
     return (
-        <div className="w-1/3 border-2 border-red-400">
-            Project Section
+<div className="flex flex-col gap-4 justify-center items-center p-4 border-2 border-blue-300 rounded-md">
+            <h1 className="text-xl font-bold">Project Section</h1>
+            <ComplexCard />
+            <ComplexCard />
+            <ComplexCard />
+            <ComplexCard />
+            
         </div>
     )
 }
