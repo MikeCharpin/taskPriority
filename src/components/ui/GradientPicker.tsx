ed9b9995ab59d20a70faca51070a11fc5d842491
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-01-20 17:54:55 -0500

    Adding gradient picker component.

---
+++ src/components/ui/GradientPicker.tsx
@@ -0,0 +1,177 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { cn } from '@/lib/utils'
+import { Paintbrush } from 'lucide-react'
+import { useMemo, useState } from 'react'
+
+export function PickerExample() {
+  const [background, setBackground] = useState('#B4D455')
+
+  return (
+    <div
+      className="w-full h-full preview flex min-h-[350px] justify-center p-10 items-center rounded !bg-cover !bg-center transition-all"
+      style={{ background }}
+    >
+      <GradientPicker background={background} setBackground={setBackground} />
+    </div>
+  )
+}
+
+export function GradientPicker({
+  background,
+  setBackground,
+  className,
+}: {
+  background: string
+  setBackground: (background: string) => void
+  className?: string
+}) {
+  const solids = [
+    '#ff75c3',
+    '#ffa647',
+    '#ffe83f',
+    '#9fff5b',
+    '#70e2ff',
+    '#cd93ff',
+    '#09203f',
+    '#be123c',
+    '#e11d48',
+    '#f9a8d4',
+    '#db2777',
+    '#c026d3',
+    '#86198f',
+    '#a855f7',
+    '#7e22ce',
+    '#8b5cf6',
+    '#6366f1',
+    '#4338ca',
+    '#2563eb',
+    '#1e40af',
+    '#075985',
+    '#0ea5e9',
+    '#06b6d4',
+    '#155e75',
+    '#14b8a6',
+    '#115e59',
+    '#059669',
+    '#065f46',
+    '#22c55e',
+    '#166534',
+    '#65a30d',
+    '#3f6212',
+    '#ca8a04',
+    '#854d0e',
+    '#d97706',
+    '#9a3412',
+    '#f97316',
+    '#b91c1c',
+    '#ef4444',
+    '#737373',
+  ]
+
+  const gradients = [
+    'linear-gradient(to top left,#accbee,#e7f0fd)',
+    'linear-gradient(to top left,#d5d4d0,#d5d4d0,#eeeeec)',
+    'linear-gradient(to top left,#000000,#434343)',
+    'linear-gradient(to top left,#09203f,#537895)',
+    'linear-gradient(to top left,#AC32E4,#7918F2,#4801FF)',
+    'linear-gradient(to top left,#f953c6,#b91d73)',
+    'linear-gradient(to top left,#ee0979,#ff6a00)',
+    'linear-gradient(to top left,#F00000,#DC281E)',
+    'linear-gradient(to top left,#00c6ff,#0072ff)',
+    'linear-gradient(to top left,#4facfe,#00f2fe)',
+    'linear-gradient(to top left,#0ba360,#3cba92)',
+    'linear-gradient(to top left,#FDFC47,#24FE41)',
+    'linear-gradient(to top left,#8a2be2,#0000cd,#228b22,#ccff00)',
+    'linear-gradient(to top left,#40E0D0,#FF8C00,#FF0080)',
+    'linear-gradient(to top left,#fcc5e4,#fda34b,#ff7882,#c8699e,#7046aa,#0c1db8,#020f75)',
+    'linear-gradient(to top left,#ff75c3,#ffa647,#ffe83f,#9fff5b,#70e2ff,#cd93ff)',
+  ]
+
+  const defaultTab = useMemo(() => {
+    if (background.includes('gradient')) return 'gradient'
+    return 'solid'
+  }, [background])
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[220px] justify-start text-left font-normal',
+            !background && 'text-muted-foreground',
+            className
+          )}
+        >
+          <div className="w-full flex items-center gap-2">
+            {background ? (
+              <div
+                className="h-4 w-4 rounded !bg-center !bg-cover transition-all"
+                style={{ background }}
+              ></div>
+            ) : (
+              <Paintbrush className="h-4 w-4" />
+            )}
+            <div className="truncate flex-1">
+              {background ? background : 'Pick a color'}
+            </div>
+          </div>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-64">
+        <Tabs defaultValue={defaultTab} className="w-full">
+          <TabsList className="w-full mb-4">
+            <TabsTrigger className="flex-1" value="solid">
+              Solid
+            </TabsTrigger>
+            <TabsTrigger className="flex-1" value="gradient">
+              Gradient
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="solid" className="flex flex-wrap gap-1 mt-0">
+            {solids.map((s) => (
+              <div
+                key={s}
+                style={{ background: s }}
+                className="rounded-md h-6 w-6 cursor-pointer active:scale-105"
+                onClick={() => setBackground(s)}
+              />
+            ))}
+          </TabsContent>
+
+          <TabsContent value="gradient" className="mt-0">
+            <div className="flex flex-wrap gap-1 mb-2">
+              {gradients.map((s) => (
+                <div
+                  key={s}
+                  style={{ background: s }}
+                  className="rounded-md h-6 w-6 cursor-pointer active:scale-105"
+                  onClick={() => setBackground(s)}
+                />
+              ))}
+            </div>
+
+          </TabsContent>
+
+        </Tabs>
+
+        <Input
+          id="custom"
+          value={background}
+          className="col-span-2 h-8 mt-4"
+          onChange={(e) => setBackground(e.currentTarget.value)}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
