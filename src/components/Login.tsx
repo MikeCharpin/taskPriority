91a3297aaddb1451d63d73e35a40a4c8f0f89958
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-02-08 16:00:36 -0500

    UPdating getURL function.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -73,15 +73,16 @@ function LoginForm({ className }: React.ComponentProps<"form">) {
     const [email, setEmail] = useState("")
     const getURL = () => {
       let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        'http://localhost:5173/'
+        process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        'http://localhost:3000/'
+
       // Make sure to include `https://` when not localhost.
       url = url.includes('http') ? url : `https://${url}`
       // Make sure to include a trailing `/`.
       url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
       return url
     }
+
     const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         console.log("sending login")
