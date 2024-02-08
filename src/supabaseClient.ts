7736bd72d107442c23fee198f734dd2e86a5f7ed
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-02-08 14:23:00 -0500

    Fixing image import path.

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,7 +4,4 @@ import { Database } from '@/database.types'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log(supabaseUrl)
-console.log(supabaseAnonKey)
-
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
