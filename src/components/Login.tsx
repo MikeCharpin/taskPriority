8cd5e3da918e9304c68054ba97fe93dcb24a04ff
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-02-08 15:45:40 -0500

    Fixing OTP redirect email.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -71,11 +71,26 @@ const Login = ({ open, setOpen }: LoginProps) => {
 function LoginForm({ className }: React.ComponentProps<"form">) {
     const [loading, setLoading] = useState(false)
     const [email, setEmail] = useState("")
+    const getURL = () => {
+      let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000/'
+      // Make sure to include `https://` when not localhost.
+      url = url.includes('http') ? url : `https://${url}`
+      // Make sure to include a trailing `/`.
+      url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+      return url
+    }
     const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         console.log("sending login")
         setLoading(true)
-        const { error } = await supabase.auth.signInWithOtp({ email })
+        const { error } = await supabase.auth.signInWithOtp({
+           email,
+            options: {
+              emailRedirectTo: getURL()
+            } })
         if (error) {
           alert(error.message)
         } else {
--- supabase/migrations/003_create_on_auth_user_created_trigger.sql
+++ supabase/migrations/003_create_on_auth_user_created_trigger.sql
@@ -3,8 +3,8 @@
 create function public.handle_new_user()
 returns trigger as $$
 begin
-  insert into public.profiles (id, full_name, avatar_url)
-  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  insert into public.profiles (id, full_name)
+  values (new.id, new.raw_user_meta_data->>'full_name');
   return new;
 end;
 $$ language plpgsql security definer;
