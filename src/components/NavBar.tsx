ebad3c908f8a588e11b0063370b4f69317d9025f
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-02-21 14:04:11 -0500

    Making Navbar responsive.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Session } from "@supabase/supabase-js"
 import NavMenuDropDown from "./NavMenuDropDown"
+import { useMediaQuery } from "@react-hook/media-query"
 
 interface NavBarProps {
     setOpenLogin: React.Dispatch<React.SetStateAction<boolean>>
@@ -9,25 +10,51 @@ interface NavBarProps {
 }
 
 const NavBar = ({ setOpenLogin, setOpenModeToggle, session }: NavBarProps) => {
+    const isDesktop = useMediaQuery("(min-width: 768px)")  
 
     
     return (
-       <nav className="flex flex-wrap w-screen items-center justify-between bg-secondary p-6">
-            <h1 className="text-2xl font-light">🌷 tulip tasks 🌷</h1>
-            {session === null ? 
-                <div className="flex flex-col  font-semibold bg-primary/20 p-4 rounded-xl">
-                    <span> 🏡 you are working locally </span>
-                    <span>login to save your data <button onClick={() => setOpenLogin(true)} >💾</button></span>
-                </div> 
-            : 
-                ""
+        <div>
+            {isDesktop ?
+            <nav className="flex flex-wrap w-screen items-center justify-between bg-secondary p-6">
+                <h1 className="text-2xl font-light">🌷 tulip tasks 🌷</h1>
+                {session === null ?
+                    <div className="flex flex-col  font-semibold bg-primary/20 p-4 rounded-xl">
+                        <span> 🏡 you are working locally </span>
+                        <span>login to save your data <button onClick={() => setOpenLogin(true)} >💾</button></span>
+                    </div>
+                :
+                    ""
+                }
+                <NavMenuDropDown
+               session={session}
+               setOpenLogin={setOpenLogin}
+               setOpenModeToggle={setOpenModeToggle}
+               />
+            </nav>
+            :
+            <nav className="flex flex-col w-screen items-center justify-between bg-secondary p-6 gap-4">
+                <div className="flex gap-6">
+                    <h1 className="text-2xl font-light">🌷 tulip tasks 🌷</h1>
+                    <NavMenuDropDown
+                        session={session}
+                        setOpenLogin={setOpenLogin}
+                        setOpenModeToggle={setOpenModeToggle}
+                    />
+                </div>
+                
+                {session === null ?
+                    <div className="flex flex-col  font-semibold bg-primary/20 p-4 rounded-xl">
+                        <span> 🏡 you are working locally </span>
+                        <span>login to save your data <button onClick={() => setOpenLogin(true)} >💾</button></span>
+                    </div>
+                :
+                    ""
+                }
+            </nav>
             }
-            <NavMenuDropDown
-           session={session}
-           setOpenLogin={setOpenLogin}
-           setOpenModeToggle={setOpenModeToggle}
-           />           
-        </nav>
+        </div>
+       
     )
 }
 
